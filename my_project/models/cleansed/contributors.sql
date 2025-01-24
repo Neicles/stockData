c53c8df9fa5b9273b6ade272e1fdc973b1d8fe93
Dorian Jaguenaud <dorian.jaguenaud@cdiscount.com>
2025-01-24 09:50:56 +0100

    /feat ajout contributors.sql

--- my_project/models/cleansed/contributors.sql
+++ my_project/models/cleansed/contributors.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
     id AS contributor_id,
-    LOWER(TRIM(login)) AS contributor_login,  -- Normalisation : login en minuscule, suppression des espaces
+    LOWER(TRIM(login)) AS contributor_login,
     contributions AS nombre_contributions
 FROM {{ source('github_data', 'contributors_data') }}
 WHERE login IS NOT NULL
